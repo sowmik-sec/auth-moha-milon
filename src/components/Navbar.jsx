b0a879d213217bcd72d6ba4f9344b87c7253dea7
sowmik-sec <sowmik.sec@gmail.com>
2024-07-09 10:53:08 +0600

    create responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ function Navbar() {
             {navLinks}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Auth Moha Milon</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{navLinks}</ul>
